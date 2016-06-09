f40ad458549d5f8e9c330758b002b2995c68c4a8
bolzai <bolzai>
2016-06-09 20:09:36 +0530

    modified files

--- index.php
+++ index.php
@@ -18,20 +18,22 @@
 </head>
 <body>
 	<div class="container bgPic">
+	<div class="layer">
 		<div class="row">
 			<div class="col-md-6 col-md-offset-3 sPos center">
-				<h1 class="fStyle center">Weather Predictor</h1>
-				<p class="lead fStyle center">Enter the name of your city to get weather updates!</p>
+			<h1 class="fStyle pStyle center">Weather Predictor</h1>
+			<p class="lead aStyle pStyle center">Want to find out what will be the weather of your city?<br />Enter the name of your city to get weather update for next 3 days!</p>
 				<form>
 					<div class="form-group">
-						<input type="text" class="form-control" name="city" id="city" placeholder="Eg. New York, London, Kolkata...">
-						<button type="submit" class="btn btn-success fStyle pStyle" id="findWeather">Find Weather</button>
+						<input type="text" class="form-control iStyle" name="city" id="city" placeholder="Eg. New York, London, Kolkata...">
+						<button type="submit" class="btn btn-success aStyle pStyle" id="findWeather">Find Weather</button>
 					</div>
 				</form>
-				<div class="alert alert-success" id="success">Success!</div>
-				<div class="alert alert-danger" id="danger">Could not find the weather for this city. Make sure it's a valid city or is not a state.</div>
+				<!-- <div class="alert alert-success" id="success">Success!</div>
+				<div class="alert alert-danger" id="danger">Could not find the weather for this city. Make sure it's a valid city or is not a state.</div> -->
 			</div>
 		</div>
+	</div>
 	</div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
